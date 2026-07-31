75d5274a2549853a2c9c1ab70209e4715c629255
TeleMati2026 <orczykowskimateusz535@gmail.com>
2026-07-31 17:54:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
   Supabase -> Project Settings -> API
 */
 window.TELEFABRYKA_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://qsztaykhvyvnalhtazsw.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_nZUxYeNze_6ahj0c3MAk6g_nJpDpVYL"
 };
